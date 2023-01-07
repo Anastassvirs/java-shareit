526833d72146fee7a64f7d04c8cab8419bde956e
Анастасия Свирская <foreducationYa@yandex.ru>
2023-01-08 01:54:58 +0300

    fix: The structure of the user folder has been changed

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,12 +1,45 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.model.User;
 
-/**
- * TODO Sprint add-controllers.
- */
+import javax.validation.Valid;
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> findAll() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{userId}")
+    public User findUser(@PathVariable Long userId) {
+        return userService.findById(userId);
+    }
+
+    @PostMapping
+    public User create(@Valid @RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping
+    public User update(@Valid @RequestBody User user) {
+        return userService.updateUser(user);
+    }
+
+    @DeleteMapping()
+    public User deleteUser(@Valid @RequestBody User user) {
+        return userService.deleteUser(user);
+    }
 }
