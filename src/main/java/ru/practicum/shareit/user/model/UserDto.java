50ddd5463e9a5a3b430dc1d3a358ce580e68f457
Анастасия Свирская <foreducationYa@yandex.ru>
2023-01-17 07:05:27 +0300

    style: fix some style problems

--- src/main/java/ru/practicum/shareit/user/model/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/model/UserDto.java
@@ -7,12 +7,13 @@ import javax.validation.constraints.NotBlank;
 
 @Data
 public class UserDto {
+    private Long id;
     @NotBlank
     private String name;
     @Email
     @NotBlank
     private String email;
-    
+
     public UserDto(String name, String email) {
         this.name = name;
         this.email = email;
