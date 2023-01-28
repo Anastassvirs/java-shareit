c1f3c1947d7fb1151339b6fbd48e8040400ca0e6
Анастасия Свирская <foreducationYa@yandex.ru>
2023-01-28 08:48:32 +0300

    fixed: UserDto don't equals User fix

--- src/main/java/ru/practicum/shareit/user/model/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/model/UserDto.java
@@ -8,8 +8,6 @@ import javax.validation.constraints.NotBlank;
 @Data
 public class UserDto {
 
-    private Long id;
-
     @NotBlank
     private String name;
 
@@ -20,17 +18,5 @@ public class UserDto {
     public UserDto(String name, String email) {
         this.name = name;
         this.email = email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof User)) return false;
-        return id != null && id.equals(((User) o).getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
     }
 }
