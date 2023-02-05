7c3dc360cc0b4d34e20d3382234258395a2bb11e
Анастасия Свирская <foreducationYa@yandex.ru>
2023-02-05 16:00:20 +0300

    fix: added stacktrace to exception handler

--- src/main/java/ru/practicum/shareit/exceptions/ExceptionShareItHandler.java
+++ src/main/java/ru/practicum/shareit/exceptions/ExceptionShareItHandler.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.StringWriter;
+import java.io.PrintWriter;
+
 @RestControllerAdvice
 public class ExceptionShareItHandler {
     @ExceptionHandler
@@ -34,7 +37,11 @@ public class ExceptionShareItHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleThrowable(final Throwable e) {
-        return new ErrorResponse("Произошла непредвиденная ошибка: " + e.getStackTrace());
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        String sStackTrace = sw.toString();
+        return new ErrorResponse("Произошла непредвиденная ошибка: " + sStackTrace);
     }
 
     @ExceptionHandler
